3b76e406e4e11f9228f7e36b77b7ffde8266bd13
Davide Ginelli <ginellidavide@gmail.com>
2021-11-04 17:04:37 +0100

    Update NopolExample.java

--- src/main/java/nopol_examples/nopol_example_3/NopolExample.java
+++ src/main/java/nopol_examples/nopol_example_3/NopolExample.java
@@ -6,13 +6,13 @@ public class NopolExample {
 	 * Return true if a is odd number
 	 */
 	public boolean isOddNumber(int a){
-		int tmp = (a-1)%2;
-		int foo = 0;
+		int tmp = (a-1)%2; 
+		int foo = 0; 
 		
-		if ( tmp != 0 ){ // Fix : tmp == 0
-			return true;
+		if ( tmp != 0 ){  // Fix : tmp == 0
+			return true; 
 		}
-		return false;
+		return false; 
 		
 	}
 	
